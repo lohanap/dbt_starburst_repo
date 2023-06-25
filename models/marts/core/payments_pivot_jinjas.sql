87b1a555a3a19d81d3c8187b79d745af69e94ae4
piyush.lohana@gmail.com <piyush.lohana@gmail.com>
2023-06-25 17:04:03 +0000

    Jinjas 2

--- models/marts/core/payments_pivot_jinjas.sql
+++ models/marts/core/payments_pivot_jinjas.sql
@@ -1,6 +1,6 @@
 select
     order_id, 
-    {% for payment_method in ['credit_card', 'coupon', 'bank_transfer', 'gift_card']  %}
+    {% for payment_method in  %}
     case when payment_method = '{{payment_method}}' and payment_status = 'success' then payment_amount else 0 end as {{payment_method}}_success,
     case when payment_method = '{{payment_method}}' then payment_amount else 0 end as {{payment_method}}_total{% if not loop.last %}, {% endif %}
     {%- endfor %}
